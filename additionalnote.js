87699d7611eb0f59f019c46543f002af529fb6d4
dilaksh05 <155356483+dilaksh05@users.noreply.github.com>
2024-01-22 09:51:47 +0530

    update

--- additionalnote.js
+++
@@ -1,6 +0,0 @@
-<script>
-   // Clear form fields when the page loads
-   window.addEventListener('DOMContentLoaded', (event) => {
-       document.querySelector('.my-form').reset(); // Replace '.my-form' with your form class or ID
-   });
-</script>
